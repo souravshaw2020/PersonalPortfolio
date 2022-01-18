19209fe1e7478f00fe1e3b6263a5f46aadd846a4
Sourav Shaw <sourav.shaw@st.niituniversity.in>
2022-01-18 19:30:30 +0530

    Header Section added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Home.css'
+import Header from '../Home/Header/Header'
 import Profile from './Profile/Profile'
 import Footer from './Footer/Footer'
 
 function Home() {
     return (
         <div className='home-body'>
+            <Header/>
             <Profile/>
             <Footer/>
         </div>
