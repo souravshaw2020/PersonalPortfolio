e60d3ef7bccd6b1eb35d825bf56e15808625140d
Sourav Shaw <sourav.shaw@st.niituniversity.in>
2023-05-20 16:20:13 +0530

    remove console log from file

--- src/components/Component.jsx
+++ src/components/Component.jsx
@@ -5,8 +5,7 @@ function Component() {
   const MapAllScreen = () => {
       return(
         TOTAL_SCREENS.map((screen) => {
-            console.log(screen);
-            return((screen.component)?<screen.component screenName={screen.screen_name}key={screen.screen_name}id={screen.screen_name}/>:<div key={screen.screen_name}></div>) 
+            return((screen.component) ? <screen.component screenName={screen.screen_name} key={screen.screen_name} id={screen.screen_name}/> : <div key={screen.screen_name}></div>) 
           })
       )
   }
